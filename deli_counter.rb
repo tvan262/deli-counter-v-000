d09c23d4070211b7262e733d45f0ac4e5cd0cbb6
Tuong Van Nguyen <tvan262@yahoo.com>
2018-04-24 19:48:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,16 @@
 def line(name)
   if name = " "
     return "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli,name)
+  puts "Welcome,#{name}. You are number #{} in line."
+end
+
+def now_serving()
+  if 
+    puts "There is nobody waiting to be served!"
+  end
+end
+
